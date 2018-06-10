89eb998911f776d94c961cc12a58752c70cfe91b
Veronika Romashkina <vrom911@gmail.com>
2018-06-10 23:41:43 +0800

    [#5] Add post template, all posts page and tags page (#10)
    * Use main logo on site and for favicon
    
    * [#5] Add post template, all posts page and tags page
    
    * Fix after review

--- site.hs
+++ site.hs
@@ -1,7 +1,10 @@
+import Data.List (nub)
 import Data.Monoid ((<>))
-import Hakyll (applyAsTemplate, compile, compressCssCompiler, copyFileCompiler, create,
-               defaultContext, hakyll, idRoute, loadAndApplyTemplate, makeItem, match,
-               relativizeUrls, route, templateBodyCompiler, (.||.))
+import Hakyll (Context, Identifier, Item (..), Pattern, Rules, applyAsTemplate, buildTags, compile,
+               compressCssCompiler, constField, copyFileCompiler, create, dateField, defaultContext,
+               field, fromCapture, getTags, hakyll, idRoute, listField, loadAll,
+               loadAndApplyTemplate, makeItem, match, pandocCompiler, recentFirst, relativizeUrls,
+               route, setExtension, tagsRules, templateBodyCompiler, (.||.))
 
 import Kowainik.Project (makeProjectContext)
 import Kowainik.Social (makeSocialContext)
@@ -29,6 +32,27 @@ main = hakyll $ do
                 >>= loadAndApplyTemplate "templates/main.html" ctx
                 >>= relativizeUrls
 
+    -- Posts pages
+    match "posts/*" $ do
+        route $ setExtension "html"
+        compile $ do
+            i   <- pandocCompiler
+            tgs <- getTags (itemIdentifier i)
+            let postTagsCtx = postCtxWithTags tgs
+            loadAndApplyTemplate "templates/post.html" postTagsCtx i
+                >>= loadAndApplyTemplate "templates/posts-default.html" postTagsCtx
+                >>= relativizeUrls
+
+    -- All posts page
+    create ["posts.html"] $ compilePosts "Posts" "templates/posts.html" "posts/*"
+
+    -- build up tags
+    tags <- buildTags "posts/*" (fromCapture "tags/*.html")
+
+    tagsRules tags $ \tag pattern -> do
+        let title = "Posts tagged \"" ++ tag ++ "\""
+        compilePosts title "templates/tag.html" pattern
+
     -- Render the 404 page, we don't relativize URL's here.
     create ["404.html"] $ do
         route idRoute
@@ -39,3 +63,32 @@ main = hakyll $ do
                 >>= loadAndApplyTemplate "templates/404.html" ctx
 
     match "templates/*" $ compile templateBodyCompiler
+
+
+compilePosts :: String -> Identifier -> Pattern -> Rules ()
+compilePosts title page pat = do
+    route idRoute
+    compile $ do
+        posts <- recentFirst =<< loadAll pat
+        let ids = map itemIdentifier posts
+        tagsList <- nub . concat <$> traverse getTags ids
+        let ctx = postCtxWithTags tagsList
+               <> constField "title" title
+               <> listField "posts" postCtx (return posts)
+               <> defaultContext
+
+        makeItem ""
+            >>= loadAndApplyTemplate page ctx
+            >>= loadAndApplyTemplate "templates/posts-default.html" ctx
+            >>= relativizeUrls
+
+
+-- Context to used for posts
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
+
+postCtxWithTags :: [String] -> Context String
+postCtxWithTags tags = listField "tagsList" (field "tag" $ pure . itemBody) (traverse makeItem tags)
+    <> postCtx
