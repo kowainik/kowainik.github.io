4cad22c90dc2e344ccc1d31d470dc665e63e748e
vrom911 <veronika@holmusk.com>
2018-06-10 22:58:36 +0800

    Fix after review

--- site.hs
+++ site.hs
@@ -32,19 +32,21 @@ main = hakyll $ do
                 >>= loadAndApplyTemplate "templates/main.html" ctx
                 >>= relativizeUrls
 
-    -- build up tags
     -- Posts pages
     match "posts/*" $ do
         route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= \i -> getTags (itemIdentifier i)
-            >>= \tgs -> loadAndApplyTemplate "templates/post.html" (postCtxWithTags tgs) i
-            >>= loadAndApplyTemplate "templates/posts-default.html" (postCtxWithTags tgs)
-            >>= relativizeUrls
+        compile $ do
+            i   <- pandocCompiler
+            tgs <- getTags (itemIdentifier i)
+            let postTagsCtx = postCtxWithTags tgs
+            loadAndApplyTemplate "templates/post.html" postTagsCtx i
+                >>= loadAndApplyTemplate "templates/posts-default.html" postTagsCtx
+                >>= relativizeUrls
 
-        -- All posts page
+    -- All posts page
     create ["posts.html"] $ compilePosts "Posts" "templates/posts.html" "posts/*"
 
+    -- build up tags
     tags <- buildTags "posts/*" (fromCapture "tags/*.html")
 
     tagsRules tags $ \tag pattern -> do
@@ -65,22 +67,20 @@ main = hakyll $ do
 
 compilePosts :: String -> Identifier -> Pattern -> Rules ()
 compilePosts title page pat = do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll pat
-            let ids = map itemIdentifier posts
-            tagsList <- nub . concat <$> traverse getTags ids
-            let ctx =  listField "tagsList2"
-                                       (field "tag2" $ pure . itemBody)
-                                       (traverse makeItem tagsList)
-                   <> constField "title" title
-                   <> listField "posts" postCtx (return posts)
-                   <> defaultContext
+    route idRoute
+    compile $ do
+        posts <- recentFirst =<< loadAll pat
+        let ids = map itemIdentifier posts
+        tagsList <- nub . concat <$> traverse getTags ids
+        let ctx = postCtxWithTags tagsList
+               <> constField "title" title
+               <> listField "posts" postCtx (return posts)
+               <> defaultContext
 
-            makeItem ""
-                >>= loadAndApplyTemplate page ctx
-                >>= loadAndApplyTemplate "templates/posts-default.html" ctx
-                >>= relativizeUrls
+        makeItem ""
+            >>= loadAndApplyTemplate page ctx
+            >>= loadAndApplyTemplate "templates/posts-default.html" ctx
+            >>= relativizeUrls
 
 
 -- Context to used for posts
