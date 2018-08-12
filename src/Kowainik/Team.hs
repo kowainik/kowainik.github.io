9c7c9c16b5ee4f1f083668b96013e164bc69841c
vrom911 <vrom911@gmail.com>
2018-08-12 21:59:49 +0800

    [#9] Pretify the team section

--- src/Kowainik/Team.hs
+++ src/Kowainik/Team.hs
@@ -24,17 +24,24 @@ memberId    = field "memberId"    $ pure . tmId    . itemBody
 
 allTeam :: Compiler [Item TeamM]
 allTeam = traverse makeItem
-    [ TeamM "Dmitry Kovanikov"
+    [ TeamM "Dmitrii Kovanikov"
             "chshersh"
-            "Haskell Developer" "dmitry.kovanikov.jpg"
-            "Functional programmer, anime lover, FP enthusiast, Idris fan and somehow is a Haskell & Compilers lecturer at the ITMO"
+            "Haskell Adept"
+            "dmitrii.kovanikov"
+            "Haskell Adept, Functional Programming enthusiast, anime lover. I like to discover elegant solutions for sophisticated software problems by squeezing maximum from the Haskell programming language. I enjoy discussions about programming languages design. Before founding Kowainik organization, I had experience in cryptocurrency and full-stack web-applications development with Haskell. Also, I was <a href='https://github.com/jagajaga/FP-Course-ITMO' target=_blank >teaching Haskell</a> to the students at the ITMO University."
             "1"
     , TeamM "Veronika Romashkina"
             "vrom911"
-            "Haskell Developer"
-            "veronika.romashkina.jpeg"
             "❤️: Haskell, 🍫, 🍷"
+            "veronika.romashkina"
+            "I'm a functional programming enthusiast who puts effort into the growth of the Haskell open source community. I'm truly passionate about Haskell. I enjoy exploring new topics and never stop learning and improving my skills."
             "2"
+    , TeamM "Vladislav Sabanov"
+            "willbasky"
+            "Haskell Developer"
+            "vladislav.sabanov"
+            "I am happy to program in Haskell, devoting much time to it. I do not leave issues until they are solved. I continuously learn and dig into the heart of things. Even my enthusiasm for generative art is associated with the coding."
+            "3"
     ]
 
 makeTeamContext :: Context a
