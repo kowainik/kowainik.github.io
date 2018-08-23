b48109721ba617bb526401969592f0aa12691507
Veronika Romashkina <vrom911@gmail.com>
2018-08-23 18:00:49 +0800

    [#29] Read team members via json file (#30)
    * [#29] Read team members via json file
    
    * Fix after review

--- src/Kowainik/Team.hs
+++ src/Kowainik/Team.hs
@@ -1,11 +1,20 @@
+-- | All team members information is in the @/team.json@ file.
+
 module Kowainik.Team
-       ( TeamM (..)
+       ( TeamMember (..)
        , makeTeamContext
+       , parseTeam
        ) where
 
-import Hakyll (Compiler, Context, Item, field, itemBody, listField, makeItem)
+import Data.Aeson (FromJSON (..), eitherDecode, withObject, (.:))
+import Data.Char (toLower)
+import Hakyll (Context, field, itemBody, listField, makeItem)
 
-data TeamM = TeamM
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.String as String (words)
+
+-- | Information about team member.
+data TeamMember = TeamMember
     { tmName  :: String
     , tmNick  :: String
     , tmWho   :: String
@@ -14,7 +23,17 @@ data TeamM = TeamM
     , tmId    :: String
     }
 
-memberName, memberNick, memberWho, memberImage, memberAbout, memberId :: Context TeamM
+instance FromJSON TeamMember where
+    parseJSON = withObject "TeamMember" $ \o -> do
+        tmName  <- o .: "name"
+        tmNick  <- o .: "nick"
+        tmWho   <- o .: "who"
+        tmAbout <- o .: "about"
+        let tmImage = intercalate "." $ String.words $ map toLower tmName
+        let tmId = tmNick
+        pure TeamMember{..}
+
+memberName, memberNick, memberWho, memberImage, memberAbout, memberId :: Context TeamMember
 memberName  = field "memberName"  $ pure . tmName  . itemBody
 memberNick  = field "memberNick"  $ pure . tmNick  . itemBody
 memberWho   = field "memberWho"   $ pure . tmWho   . itemBody
@@ -22,30 +41,17 @@ memberImage = field "memberImage" $ pure . tmImage . itemBody
 memberAbout = field "memberAbout" $ pure . tmAbout . itemBody
 memberId    = field "memberId"    $ pure . tmId    . itemBody
 
-allTeam :: Compiler [Item TeamM]
-allTeam = traverse makeItem
-    [ TeamM "Dmitrii Kovanikov"
-            "chshersh"
-            "Haskell Adept"
-            "dmitrii.kovanikov"
-            "Haskell Adept, Functional Programming enthusiast, anime lover. I like to discover elegant solutions for sophisticated software problems by squeezing maximum from the Haskell programming language. I enjoy discussions about programming languages design. Before founding Kowainik organization, I had experience in cryptocurrency and full-stack web-applications development with Haskell. Also, I was <a href='https://github.com/jagajaga/FP-Course-ITMO' target=_blank >teaching Haskell</a> to the students at the ITMO University."
-            "1"
-    , TeamM "Veronika Romashkina"
-            "vrom911"
-            "❤️: Haskell, 🍫, 🍷"
-            "veronika.romashkina"
-            "I'm a functional programming enthusiast who puts effort into the growth of the Haskell open source community. I'm truly passionate about Haskell. I enjoy exploring new topics and never stop learning and improving my skills."
-            "2"
-    , TeamM "Vladislav Sabanov"
-            "willbasky"
-            "Haskell Developer"
-            "vladislav.sabanov"
-            "I am happy to program in Haskell, devoting much time to it. I do not leave issues until they are solved. I continuously learn and dig into the heart of things. Some of my hobbies are live coding and generative art."
-            "3"
-    ]
+makeTeamContext :: [TeamMember] -> Context a
+makeTeamContext = listField "team"
+    ( memberName  <> memberNick
+   <> memberImage <> memberAbout
+   <> memberId    <> memberWho )
+    . traverse makeItem
 
-makeTeamContext :: Context a
-makeTeamContext = listField "team" ( memberName  <> memberNick
-                                  <> memberImage <> memberAbout
-                                  <> memberId    <> memberWho )
-                                   allTeam
+parseTeam :: FilePath -> IO [TeamMember]
+parseTeam teamPath = do
+    teamJson <- LBS.readFile teamPath
+    let eitherTeam = eitherDecode teamJson
+    case eitherTeam of
+        Left e     -> error $ toText e
+        Right team -> return team
