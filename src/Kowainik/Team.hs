49f3198b42724eea3ccd52f8d7a1994db32cd194
Veronika Romashkina <vrom911@gmail.com>
2020-11-01 23:35:33 +0000

    [#79] Links to websites of teammembers (#139)
    * [#79] Links to websites of teammembers
    
    Resolves #79
    
    * Update src/Kowainik/Team.hs
    
    Co-authored-by: hint-man[bot] <44720633+hint-man[bot]@users.noreply.github.com>
    
    Co-authored-by: hint-man[bot] <44720633+hint-man[bot]@users.noreply.github.com>

--- src/Kowainik/Team.hs
+++ src/Kowainik/Team.hs
@@ -1,14 +1,14 @@
 -- | All team members information is in the @/team.json@ file.
 
 module Kowainik.Team
-       ( TeamMember (..)
-       , makeTeamContext
-       , makeCreatorsContext
-       , makeVolunteersContext
-       , parseTeam
-       ) where
+    ( TeamMember (..)
+    , makeTeamContext
+    , makeCreatorsContext
+    , makeVolunteersContext
+    , parseTeam
+    ) where
 
-import Data.Aeson (FromJSON (..), eitherDecode, withObject, (.:))
+import Data.Aeson (FromJSON (..), eitherDecode, withObject, (.:), (.:?), (.!=))
 import Data.Char (toLower)
 import Hakyll (Context, field, itemBody, listField, makeItem)
 
@@ -17,31 +17,37 @@ import qualified Data.String as String (words)
 
 -- | Information about team member.
 data TeamMember = TeamMember
-    { tmName  :: String
-    , tmNick  :: String
-    , tmWho   :: String
-    , tmImage :: String
-    , tmAbout :: String
-    , tmId    :: String
+    { tmName    :: String
+    , tmNick    :: String
+    , tmWho     :: String
+    , tmWeb     :: Maybe String
+    , tmImage   :: String
+    , tmAbout   :: String
+    , tmSponsor :: Bool
+    , tmId      :: String
     }
 
 instance FromJSON TeamMember where
     parseJSON = withObject "TeamMember" $ \o -> do
-        tmName  <- o .: "name"
-        tmNick  <- o .: "nick"
-        tmWho   <- o .: "who"
-        tmAbout <- o .: "about"
+        tmName    <- o .: "name"
+        tmNick    <- o .: "nick"
+        tmWho     <- o .: "who"
+        tmWeb     <- o .:? "web"
+        tmAbout   <- o .: "about"
+        tmSponsor <- o .:? "sponsor" .!= False
         let tmImage = intercalate "." $ String.words $ map toLower tmName
         let tmId = tmNick
         pure TeamMember{..}
 
-memberName, memberNick, memberWho, memberImage, memberAbout, memberId :: Context TeamMember
+memberName, memberNick, memberWho, memberWeb, memberImage, memberAbout, memberSponsor, memberId :: Context TeamMember
 memberName  = field "memberName"  $ pure . tmName  . itemBody
 memberNick  = field "memberNick"  $ pure . tmNick  . itemBody
 memberWho   = field "memberWho"   $ pure . tmWho   . itemBody
 memberImage = field "memberImage" $ pure . tmImage . itemBody
 memberAbout = field "memberAbout" $ pure . tmAbout . itemBody
 memberId    = field "memberId"    $ pure . tmId    . itemBody
+memberWeb   = field "memberWeb"   $ maybe empty pure . tmWeb . itemBody
+memberSponsor = field "memberSponsor" $ (\p -> if p then pure "" else empty) . tmSponsor . itemBody
 
 makeVolunteersContext :: [TeamMember] -> Context a
 makeVolunteersContext = makeTeamContext "volunteers"
@@ -51,9 +57,15 @@ makeCreatorsContext = makeTeamContext "creators"
 
 makeTeamContext :: String -> [TeamMember] -> Context a
 makeTeamContext txt = listField txt
-    ( memberName  <> memberNick
-   <> memberImage <> memberAbout
-   <> memberId    <> memberWho )
+    ( memberName
+    <> memberNick
+    <> memberImage
+    <> memberAbout
+    <> memberId
+    <> memberWho
+    <> memberWeb
+    <> memberSponsor
+    )
     . traverse makeItem
 
 parseTeam :: FilePath -> IO [TeamMember]
